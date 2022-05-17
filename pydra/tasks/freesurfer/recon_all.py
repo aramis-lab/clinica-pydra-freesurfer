85cb80a3ce0b94a32ed4dc1763f32aab9d8761f0
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-05-17 17:52:33 +0200

    Implement FreeSurfer longitudinal interface

--- pydra/tasks/freesurfer/recon_all.py
+++ pydra/tasks/freesurfer/recon_all.py
@@ -1,19 +1,81 @@
-from enum import Enum
-
-from pydra.engine.specs import ShellSpec, SpecInfo
-
+from typing import List, Tuple
+from pydra.engine.specs import ShellSpec, SpecInfo, ShellOutSpec
 from pydra import ShellCommandTask
 
 __all__ = ("ReconAll",)
 
 
-ReconAllInputSpec = SpecInfo(
-    name="ReconAllInputs",
-    fields=[
-        ("subject", str, dict(argstr="-s {subject}", help_string="Subject identifier")),
-        ("directive", str, dict(argstr="-{directive}", help_string="Workflow directive", position=-1)),
-    ],
-    bases=(ShellSpec,),
+input_fields = [
+    (
+        "subject",
+        str,
+        {
+            "help_string": "subject identifier",
+            "argstr": "-s {subject}",
+        },
+    ),
+    (
+        "base_subject",
+        str,
+        {
+            "help_string": "base subject template",
+            "argstr": "-base {base_subject}",
+        },
+    ),
+    (
+        "longitudinal_timepoint_id",
+        str,
+        {
+            "help_string": "longitudinal timepoint identifier",
+            "argstr": "-long {longitudinal_timepoint_id} {longitudinal_template_id}",
+            "requires": ["longitudinal_template_id"],
+        },
+    ),
+    (
+        "longitudinal_template_id",
+        str,
+        {
+            "help_string": "longitudinal template identifier",
+            "argstr": None,
+            "requires": ["longitudinal_timepoint_id"],
+        }
+    ),
+    (
+        "timepoints",
+        List[str],
+        {
+            "help_string": "timepoints",
+            "argstr": "-tp...",
+        },
+    ),
+    (
+        "input_file",
+        str,
+        {
+            "help_string": "input NIfTI or DICOM file",
+            "argstr": "-i {input_file}",
+        },
+    ),
+    (
+        "directive",
+        str,
+        {
+            "help_string": "workflow directive",
+            "argstr": "-{directive}",
+            "allowed_values": ["all"],
+            "position": -1,
+        },
+    ),
+]
+
+recon_all_input_spec = SpecInfo(
+    name="Input", fields=input_fields, bases=(ShellSpec,)
+)
+
+output_fields = []
+
+recon_all_output_spec = SpecInfo(
+    name="Output", fields=output_fields, bases=(ShellOutSpec,)
 )
 
 
@@ -24,13 +86,21 @@ class ReconAll(ShellCommandTask):
 
     Examples
     --------
-    >>> task = ReconAll()
-    >>> task.inputs.subject = "sub-01"
-    >>> task.inputs.directive = "all"
+    >>> task = ReconAll(directive="all", subject="tpNid", input_file="path_to_tpN_dcm")
     >>> task.cmdline
-    'recon-all -s sub-01 -all'
+    'recon-all -s tpNid -i path_to_tpN_dcm -all'
+    >>> task = ReconAll(directive="all", base_subject="templateid", timepoints=["tp1id", "tp2id"])
+    >>> task.cmdline
+    'recon-all -base templateid -tp tp1id -tp tp2id -all'
+    >>> task = ReconAll(
+    ...    directive="all",
+    ...    longitudinal_timepoint_id="tpNid",
+    ...    longitudinal_template_id="templateid",
+    ... )
+    >>> task.cmdline
+    'recon-all -long tpNid templateid -all'
     """
 
-    input_spec = ReconAllInputSpec
-    output_spec = None
+    input_spec = recon_all_input_spec
+    output_spec = recon_all_output_spec
     executable = "recon-all"
