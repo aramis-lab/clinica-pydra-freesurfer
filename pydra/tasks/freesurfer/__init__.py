d87c92f0a9be3e81421733ca769b0ac702a81035
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-05-18 20:25:27 +0200

    Expose GTMSeg to namespace

--- pydra/tasks/freesurfer/__init__.py
+++ pydra/tasks/freesurfer/__init__.py
@@ -5,6 +5,7 @@ Import Pydra's engine and FreeSurfer's tasks.
 >>> import pydra.engine
 >>> import pydra.tasks.freesurfer
 """
+from .gtmseg import GTMSeg
 from .recon_all import ReconAll
 
-__all__ = (ReconAll,)
+__all__ = [GTMSeg, ReconAll]
