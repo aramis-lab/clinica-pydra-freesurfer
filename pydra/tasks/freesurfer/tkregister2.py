32e7bd00fdcc209b34bb9bdc8420fbf76e69a32e
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-10-03 20:10:13 +0200

    Add first example

--- pydra/tasks/freesurfer/tkregister2.py
+++ pydra/tasks/freesurfer/tkregister2.py
@@ -10,11 +10,70 @@ class TkRegister2(ShellCommandTask):
 
     tkregister2 is a tool to assist in the manual tuning of the linear registration between two volumes, mainly for the
     purpose of interacting with the FreeSurfer anatomical stream.
+
+    Example
+    -------
+
+    1. create a registration matrix between the conformed space (orig.mgz) and the native anatomical (rawavg.mgz):
+
+    >>> task = TkRegister2(
+    ...     moving_volume="rawavg.mgz",
+    ...     target_volume="orig.mgz",
+    ...     registration_file="register.native.dat",
+    ...     init_registration_from_headers=True,
+    ... )
+    >>> task.cmdline
+    'tkregister2 --noedit --mov rawavg.mgz --targ orig.mgz --reg register.native.dat --regheader'
     """
 
     input_spec = SpecInfo(
         name="TkRegister2Input",
-        fields=[],
+        fields=[
+            (
+                "moving_volume",
+                str,
+                {
+                    "help_string": "moving volume",
+                    "mandatory": True,
+                    "argstr": "--mov {moving_volume}",
+                },
+            ),
+            (
+                "target_volume",
+                str,
+                {
+                    "help_string": "target volume",
+                    "argstr": "--targ {target_volume}",
+                    "xor": {"fs_target_volume"},
+                },
+            ),
+            (
+                "fs_target_volume",
+                bool,
+                {
+                    "help_string": "use T1 volume from subject found in the input registration matrix",
+                    "argstr": "--fstarg",
+                    "xor": {"target_volume"},
+                },
+            ),
+            (
+                "registration_file",
+                str,
+                {
+                    "help_string": "input or output registration file",
+                    "argstr": "--reg {registration_file}",
+                    "output_file_template": "{registration_file}",
+                },
+            ),
+            (
+                "init_registration_from_headers",
+                bool,
+                {
+                    "help_string": "compute the initial registration from the input volumes headers",
+                    "argstr": "--regheader",
+                },
+            ),
+        ],
         bases=(ShellSpec,),
     )
 
@@ -24,4 +83,4 @@ class TkRegister2(ShellCommandTask):
         bases=(ShellOutSpec,),
     )
 
-    executable = "tkregister2"
+    executable = "tkregister2 --noedit"
