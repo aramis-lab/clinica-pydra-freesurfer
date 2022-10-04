19fe131cf9cb9bd339ca34c9ae8c192cd6240f04
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-10-04 13:42:01 +0200

    Add mandatory positional arguments

--- pydra/tasks/freesurfer/mris_expand.py
+++ pydra/tasks/freesurfer/mris_expand.py
@@ -14,7 +14,38 @@ class MRISExpand(ShellCommandTask):
 
     input_spec = SpecInfo(
         name="MRISExpandInput",
-        fields=[],
+        fields=[
+            (
+                "input_surface",
+                str,
+                {
+                    "help_string": "input surface file",
+                    "mandatory": True,
+                    "argstr": "{input_surface}",
+                    "position": -3,
+                },
+            ),
+            (
+                "distance",
+                float,
+                {
+                    "help_string": "distance in mm or fraction of cortical thickness",
+                    "mandatory": True,
+                    "argstr": "{distance}",
+                    "position": -2,
+                },
+            ),
+            (
+                "output_surface",
+                str,
+                {
+                    "help_string": "output surface file",
+                    "mandatory": True,
+                    "argstr": "{output_surface}",
+                    "position": -1,
+                },
+            ),
+        ],
         bases=(ShellSpec,),
     )
 
