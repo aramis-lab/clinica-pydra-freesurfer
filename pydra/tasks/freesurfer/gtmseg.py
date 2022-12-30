87a9e64268d1c0c124d8215a06c7982232b1f940
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-12-30 19:41:56 +0100

    REF: Define GTMSeg input spec as a class

--- pydra/tasks/freesurfer/gtmseg.py
+++ pydra/tasks/freesurfer/gtmseg.py
@@ -1,8 +1,112 @@
+import attrs
+
 import pydra
 
 from . import specs
 
 __all__ = ["GTMSeg"]
+
+
+@attrs.define(slots=False, kw_only=True)
+class GTMSegSpec(pydra.specs.ShellSpec):
+    subject_id: str = attrs.field(
+        metadata={
+            "help_string": "subject to analyze",
+            "mandatory": True,
+            "argstr": "--s {subject_id}",
+        }
+    )
+
+    output_volume: str = attrs.field(
+        default="gtmseg.mgz",
+        metadata={
+            "help_string": "output volume relative to subject's mri directory",
+            "argstr": "--o {output_volume}",
+        },
+    )
+
+    xcerseg: bool = attrs.field(
+        metadata={
+            "help_string": "(re)generate or use apas+head.mgz",
+            "mandatory": True,
+            # See https://github.com/nipype/pydra/issues/611
+            "formatter": (
+                lambda field: ""
+                if field is None
+                else "--xcerseg"
+                if field
+                else "--no-xcerseg"
+            ),
+            "xor": {"headseg"},
+        }
+    )
+
+    headseg: str = attrs.field(
+        metadata={
+            "help_string": "use custom headseg instead of apas+head.mgz",
+            "mandatory": True,
+            "argstr": "--head {headseg}",
+            "xor": {"xcerseg"},
+        }
+    )
+
+    no_pons: bool = attrs.field(
+        metadata={
+            "help_string": "no pons segmentation with xcerebralseg",
+            "argstr": "--no-pons",
+            "requires": ["xcerseg"],
+        }
+    )
+
+    no_vermis: bool = attrs.field(
+        metadata={
+            "help_string": "no vermis segmentation with xcerebralseg",
+            "argstr": "--no-vermis",
+            "requires": ["xcerseg"],
+        }
+    )
+
+    colortable: str = attrs.field(
+        metadata={
+            "help_string": "use custom colortable",
+            "argstr": "--ctab {colortable}",
+        }
+    )
+
+    upsampling_factor: int = attrs.field(
+        metadata={
+            "help_string": "upsampling factor (defaults to 2)",
+            "argstr": "--usf {upsampling_factor}",
+        }
+    )
+
+    output_upsampling_factor: int = attrs.field(
+        metadata={
+            "help_string": "output upsampling factor (if different from upsampling factor)",
+            "argstr": "--output-usf {output_upsampling_factor}",
+        }
+    )
+
+    keep_hypointensities: bool = attrs.field(
+        metadata={
+            "help_string": "do not relabel hypointensities as white matter",
+            "argstr": "--keep-hypo",
+        }
+    )
+
+    keep_corpus_callosum: bool = attrs.field(
+        metadata={
+            "help_string": "do not relabel corpus callosum as white matter",
+            "argstr": "--keep-cc",
+        }
+    )
+
+    subsegment_white_matter: bool = attrs.field(
+        metadata={
+            "help_string": "subsegment white matter into lobes",
+            "argstr": "--subsegwm",
+        }
+    )
 
 
 class GTMSeg(pydra.ShellCommandTask):
@@ -35,120 +139,7 @@ class GTMSeg(pydra.ShellCommandTask):
 
     input_spec = pydra.specs.SpecInfo(
         name="GTMSegInput",
-        fields=[
-            (
-                "subject_id",
-                str,
-                {
-                    "help_string": "subject to analyze",
-                    "mandatory": True,
-                    "argstr": "--s {subject_id}",
-                },
-            ),
-            (
-                "output_volume",
-                str,
-                "gtmseg.mgz",
-                {
-                    "help_string": "output volume relative to subject's mri directory",
-                    "argstr": "--o {output_volume}",
-                },
-            ),
-            (
-                "xcerseg",
-                bool,
-                {
-                    "help_string": "(re)generate or use apas+head.mgz",
-                    "mandatory": True,
-                    # See https://github.com/nipype/pydra/issues/611
-                    "formatter": (
-                        lambda field: ""
-                        if field is None
-                        else "--xcerseg"
-                        if field
-                        else "--no-xcerseg"
-                    ),
-                    "xor": {"headseg"},
-                },
-            ),
-            (
-                "headseg",
-                str,
-                {
-                    "help_string": "use custom headseg instead of apas+head.mgz",
-                    "mandatory": True,
-                    "argstr": "--head {headseg}",
-                    "xor": {"xcerseg"},
-                },
-            ),
-            (
-                "no_pons",
-                bool,
-                {
-                    "help_string": "no pons segmentation with xcerebralseg",
-                    "argstr": "--no-pons",
-                    "requires": ["xcerseg"],
-                },
-            ),
-            (
-                "no_vermis",
-                bool,
-                {
-                    "help_string": "no vermis segmentation with xcerebralseg",
-                    "argstr": "--no-vermis",
-                    "requires": ["xcerseg"],
-                },
-            ),
-            (
-                "colortable",
-                str,
-                {
-                    "help_string": "custom colortable",
-                    "argstr": "--ctab {colortable}",
-                },
-            ),
-            (
-                "upsampling_factor",
-                int,
-                {
-                    "help_string": "upsampling factor (defaults to 2)",
-                    "argstr": "--usf {upsampling_factor}",
-                },
-            ),
-            (
-                "output_upsampling_factor",
-                int,
-                {
-                    "help_string": "output upsampling factor (if different from upsampling factor)",
-                    "argstr": "--output-usf {output_upsampling_factor}",
-                },
-            ),
-            (
-                "keep_hypointensities",
-                bool,
-                {
-                    "help_string": "do not relabel hypointensities as white matter",
-                    "argstr": "--keep-hypo",
-                },
-            ),
-            (
-                "keep_corpus_callosum",
-                bool,
-                {
-                    "help_string": "do not relabel corpus callosum as white matter",
-                    "argstr": "--keep-cc",
-                },
-            ),
-            (
-                "subsegment_white_matter",
-                bool,
-                {
-                    "help_string": "subsegment white matter into lobes",
-                    "argstr": "--subsegwm",
-                },
-            ),
-        ],
-        bases=(specs.SubjectsDirSpec,),
+        bases=(GTMSegSpec, specs.SubjectsDirSpec),
     )
 
     output_spec = pydra.specs.SpecInfo(
