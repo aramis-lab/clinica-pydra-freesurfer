4067bad82012cb385cdfe487174d29a1e5d6f2da
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-01-04 20:29:58 +0100

    DOC: Replace numpydoc by napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ release = '0.0.3'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['numpydoc']
+extensions = [
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
