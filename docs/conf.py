39db4860fdb32eb4f354410903725a164d8e8e5d
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-01-11 21:32:38 +0100

    MNT: Bump version to 0.0.8

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'pydra-freesurfer'
 author = 'The Aramis Lab'
 copyright = f'2022-2023, {author}'
-release = '0.0.7'
+release = '0.0.8'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
