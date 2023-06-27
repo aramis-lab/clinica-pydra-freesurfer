78804018f4d17a15bc00a4da607a9f45d23712c8
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-06-27 09:02:37 +0200

    FIX: Fix doctests of recon_all module

--- src/pydra/tasks/freesurfer/recon_all/__init__.py
+++ src/pydra/tasks/freesurfer/recon_all/__init__.py
@@ -9,28 +9,19 @@ Examples
 
 1. Cross-sectionally process timepoints:
 
->>> task = ReconAll(
-...     subject_id="tp1",
-...     t1_volume_file="/path/to/tp1.dcm",
-... )
+>>> task = ReconAll(subject_id="tp1", t1_volume="/path/to/tp1.dcm")
 >>> task.cmdline
 'recon-all -subjid tp1 -i /path/to/tp1.dcm -all'
 
 2. Create and process the unbiased base template:
 
->>> task = BaseReconAll(
-...     base_template_id="longbase",
-...     base_timepoint_ids=["tp1", "tp2"],
-... )
+>>> task = BaseReconAll(base_template_id="longbase", base_timepoint_ids=["tp1", "tp2"])
 >>> task.cmdline
 'recon-all -base longbase -base-tp tp1 -base-tp tp2 -all'
 
 3. Longitudinally process timepoints:
 
->>> task = LongReconAll(
-...    longitudinal_timepoint_id="tp1",
-...    longitudinal_template_id="longbase",
-... )
+>>> task = LongReconAll(longitudinal_timepoint_id="tp1", longitudinal_template_id="longbase")
 >>> task.cmdline
 'recon-all -long tp1 longbase -all'
 """
