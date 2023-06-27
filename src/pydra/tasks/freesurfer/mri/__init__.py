cdab14069f499fc94fdd279e1663ce8fa72d9959
Ghislain Vaillant <ghisvail@users.noreply.github.com>
2023-06-27 19:12:34 +0200

    NEW: Add task for mri_coreg (#65)
    * NEW: Add task for mri_coreg
    
    * MNT: Add doctest for Coreg

--- src/pydra/tasks/freesurfer/mri/__init__.py
+++ src/pydra/tasks/freesurfer/mri/__init__.py
@@ -5,6 +5,7 @@ Volume Utilities
 .. automodule:: pydra.tasks.freesurfer.mri.aparc2aseg
 .. automodule:: pydra.tasks.freesurfer.mri.binarize
 .. automodule:: pydra.tasks.freesurfer.mri.convert
+.. automodule:: pydra.tasks.freesurfer.mri.coreg
 .. automodule:: pydra.tasks.freesurfer.mri.label2vol
 .. automodule:: pydra.tasks.freesurfer.mri.surf2surf
 .. automodule:: pydra.tasks.freesurfer.mri.vol2vol
@@ -12,6 +13,7 @@ Volume Utilities
 from .aparc2aseg import Aparc2Aseg
 from .binarize import Binarize
 from .convert import Convert
+from .coreg import Coreg
 from .label2vol import Label2Vol
 from .surf2surf import Surf2Surf
 from .vol2vol import Vol2Vol
---
+++ src/pydra/tasks/freesurfer/mri/coreg.py
@@ -0,0 +1,74 @@
+"""
+Coreg
+=====
+
+Perform linear registration between two volumes similar to SPM's spm_coreg.
+
+Examples
+--------
+
+>>> task = Coreg(source_volume="template.nii", target_volume="orig.mgz", degrees_of_freedom=12)
+>>> task.cmdline    # doctest: +ELLIPSIS
+'mri_coreg --mov template.nii --ref orig.mgz --reg .../template_coreg.lta --regdat .../template_coreg.dat --dof 12 ...'
+"""
+
+__all__ = ["Coreg"]
+
+from os import PathLike
+
+from attrs import define, field
+from pydra.engine.specs import ShellSpec, SpecInfo
+from pydra.engine.task import ShellCommandTask
+
+
+@define(kw_only=True)
+class CoregSpec(ShellSpec):
+    """Specifications for mri_coreg."""
+
+    source_volume: PathLike = field(metadata={"help_string": "source volume", "mandatory": True, "argstr": "--mov"})
+
+    target_volume: PathLike = field(metadata={"help_string": "target volume", "mandatory": True, "argstr": "--ref"})
+
+    output_registration_file: str = field(
+        metadata={
+            "help_string": "output registration file",
+            "argstr": "--reg",
+            "output_file_template": "{source_volume}_coreg.lta",
+            "keep_extension": False,
+        }
+    )
+
+    output_registration_data: str = field(
+        metadata={
+            "help_string": "output registration data",
+            "argstr": "--regdat",
+            "output_file_template": "{source_volume}_coreg.dat",
+            "keep_extension": False,
+        }
+    )
+
+    subject_id: str = field(
+        metadata={"help_string": "use subject's aparc+aseg.mgz as target mask", "argstr": "--s", "xor": {"target_mask"}}
+    )
+
+    degrees_of_freedom: int = field(default=6, metadata={"help_string": "degrees of freedom", "argstr": "--dof"})
+
+    source_mask: PathLike = field(metadata={"help_string": "mask for source volume", "argstr": "--mov-mask"})
+
+    target_mask: PathLike = field(
+        metadata={"help_string": "mask for target volume", "argstr": "--ref-mask", "xor": {"subject_id"}}
+    )
+
+    num_threads: int = field(metadata={"help_string": "number of threads", "argstr": "--threads"})
+
+    subjects_dir: PathLike = field(metadata={"help_string": "subjects directory", "argstr": "--sd"})
+
+    random_seed: int = field(default=53, metadata={"help_string": "random seed", "argstr": "--seed"})
+
+
+class Coreg(ShellCommandTask):
+    """Task definition for mri_coreg."""
+
+    executable = "mri_coreg"
+
+    input_spec = SpecInfo(name="Input", bases=(CoregSpec,))
